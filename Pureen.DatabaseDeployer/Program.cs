fd26cb37089a63fced2e85f949d33fd8b69acfaf
Edwin Zelaya <edwin_zelaya5@unitec.edu>
2014-02-18 17:50:27 -0600

    Fixd connection request

--- Pureen.DatabaseDeployer/Program.cs
+++ Pureen.DatabaseDeployer/Program.cs
@@ -18,7 +18,7 @@ namespace Pureen.DatabaseDeployer
         static void Main(string[] args)
         {
             MsSqlConfiguration databaseConfiguration = MsSqlConfiguration.MsSql2008.ShowSql().
-               ConnectionString(x => x.FromConnectionStringWithKey("Local"));
+               ConnectionString(x => x.FromConnectionStringWithKey("Remote"));
 
             DomainDrivenDatabaseDeployer.DatabaseDeployer dd = null;
             ISessionFactory sessionFactory = new SessionFactoryBuilder(new MappingScheme(), databaseConfiguration)
